7da4041aeb2f06691fcf18fc483ee8012447370c
Jess on the Hope <jesshon.py326@gmail.com>
2022-03-27 18:23:54 -0400

    connection manager, discovering peers
    setting up connection manager to discover nearby peers.  waits for testing.

--- peerConnect/peerConnect/DiscoveredDevicesViewController.swift
+++ peerConnect/peerConnect/DiscoveredDevicesViewController.swift
@@ -6,20 +6,52 @@
 //
 
 import UIKit
+import Foundation
+import SwiftUI
 
 
 class DiscoveredDevicesViewController: UIViewController {
     
     @IBOutlet weak var devicesTableView: UITableView!
     
-    private var connectionManager: ConnectionManager!
+    @ObservedObject var connectionManager = ConnectionManager()
+    
+    //init() {
+        //connectionManager = ConnectionManager()
+    //}
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        connectionManager = ConnectionManager()
-        connectionManager.
+        //connectionManager = ConnectionManager()
+        connectionManager.startBrowsing()
+        
     }
     
 
+}
+
+extension DiscoveredDevicesViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        self.connectionManager.peers.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell : PeerCell! = devicesTableView.dequeueReusableCell(withIdentifier: "PeerCellUnit") as? PeerCell
+        
+        let peer = self.connectionManager.peers[(indexPath as IndexPath).row]
+        
+        cell.peerName?.text = peer.displayName
+        return cell
+    }
+    
+    
+}
+
+class PeerCell: UITableViewCell {
+    
+   
+    @IBOutlet weak var peerName: UILabel!
+    
 }
